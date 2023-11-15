ce530510575b61300ca3ab37d9c3177feb3c5af3
Jessamine McHugh <122129438+JessamineCodes@users.noreply.github.com>
2023-11-15 21:36:50 +0000

    prepared up to where livecode will start

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,25 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
